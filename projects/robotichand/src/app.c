0d0656a0f446f23ff9491881ddf874164fe0397e
Francisco Perez <pacofr2000@gmail.com>
2023-09-21 19:17:02 -0300

    first program to debug usb connection

--- projects/robotichand/src/app.c
+++ projects/robotichand/src/app.c
@@ -20,12 +20,25 @@
 
 /*==================[external functions definition]==========================*/
 
+uint8_t receivedData;
+
 int main(void)
 {
 	boardConfig(); //Inicializacion placa EDU-CIAA-NXP
 	uartConfig(UART_USB, 115200); //Configuracion de USB a 115200 baud
 
+	//delay_t delay;
+	//delayConfig(&delay, 500); // 0,5 ms retardo no bloqueante
+
 	while(1){
+
+
+			 uartReadByte(UART_USB, &receivedData);
+			 	if(receivedData != 0){
+			 		gpioWrite(LED_1, ON);
+			 	} else {
+			 		gpioWrite(LED_1, OFF);
+			 	}
 
 	}
 
